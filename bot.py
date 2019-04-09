5c7b91fc7633af5cda7c900bfe34bbaa1e7a5d93
egor5q <31898567+egor5q@users.noreply.github.com>
2019-04-09 23:39:06 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -78,46 +78,50 @@ def dd(m):
 def inline(call):
   try:
     user=call.from_user
-    game=polls[int(call.data.split(' ')[1])]
-    if user.id not in game['users'] and call.data!='xyi':
-        if 'penis' in call.data:
-            dick=True
-            bot.answer_callback_query(call.id, '🍆|Ура! Вы выбрали ящик с членом!', show_alert=True)
-        else:
-            dick=False
-            bot.answer_callback_query(call.id, '💨|О нет! Вы выбрали ящик без члена!', show_alert=True)
-        
-        game['users'].update({user.id:{'name':call.from_user.first_name,
-                                      'dick':dick}})
-        kb=types.InlineKeyboardMarkup(3)
-        
-        medit(editmsg(game), call.message.chat.id, call.message.message_id, reply_markup=game['kb'])
-        
-    elif 'endgame' in call.data:
-        kb2=types.InlineKeyboardMarkup()
-        buttons1=[]
-        buttons2=[]
-        buttons3=[]
-        i=1
-        while i<=9:
-            if i in game['dicks']:
-                emoj='🍆'
+    try:
+        game=polls[int(call.data.split(' ')[1])]
+    except:
+        game=None
+    if game!=None:
+        if user.id not in game['users'] and call.data!='xyi':
+            if 'penis' in call.data:
+                dick=True
+                bot.answer_callback_query(call.id, '🍆|Ура! Вы выбрали ящик с членом!', show_alert=True)
             else:
-                emoj='💨'
-            if i<=3:
-                buttons1.append(types.InlineKeyboardButton(text=emoj, callback_data='xyi'))
-            elif i<=6:
-                buttons2.append(types.InlineKeyboardButton(text=emoj, callback_data='xyi'))
-            elif i<=9:
-                buttons3.append(types.InlineKeyboardButton(text=emoj, callback_data='xyi'))
-            i+=1
-        kb2.add(*buttons1)
-        kb2.add(*buttons2)
-        kb2.add(*buttons3)
-        result=editmsg(game)
-        medit('Игра окончена юзером '+call.from_user.first_name+'! Результаты:\n'+result, call.message.chat.id, call.message.message_id, reply_markup=kb2)
-    else:
-        bot.answer_callback_query(call.id, 'Вы уже походили!')
+                dick=False
+                bot.answer_callback_query(call.id, '💨|О нет! Вы выбрали ящик без члена!', show_alert=True)
+            
+            game['users'].update({user.id:{'name':call.from_user.first_name,
+                                          'dick':dick}})
+            kb=types.InlineKeyboardMarkup(3)
+            
+            medit(editmsg(game), call.message.chat.id, call.message.message_id, reply_markup=game['kb'])
+        
+        elif 'endgame' in call.data:
+            kb2=types.InlineKeyboardMarkup()
+            buttons1=[]
+            buttons2=[]
+            buttons3=[]
+            i=1
+            while i<=9:
+                if i in game['dicks']:
+                    emoj='🍆'
+                else:
+                    emoj='💨'
+                if i<=3:
+                    buttons1.append(types.InlineKeyboardButton(text=emoj, callback_data='xyi'))
+                elif i<=6:
+                    buttons2.append(types.InlineKeyboardButton(text=emoj, callback_data='xyi'))
+                elif i<=9:
+                    buttons3.append(types.InlineKeyboardButton(text=emoj, callback_data='xyi'))
+                i+=1
+            kb2.add(*buttons1)
+            kb2.add(*buttons2)
+            kb2.add(*buttons3)
+            result=editmsg(game)
+            medit('Игра окончена юзером '+call.from_user.first_name+'! Результаты:\n'+result, call.message.chat.id, call.message.message_id, reply_markup=kb2)
+        else:
+            bot.answer_callback_query(call.id, 'Вы уже походили!')
   except Exception as e:
     bot.send_message(441399484, traceback.format_exc())
     
